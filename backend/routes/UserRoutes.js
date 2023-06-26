ab2a818f114b269c0ec1c9d4863854211a31a81f
Julio Machado <jsmacbusiness@gmail.com>
2023-06-26 15:57:45 -0300

    Feat: user update middleware

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -13,12 +13,22 @@ const validate = require("../middlewares/handleValidation");
 const {
   userCreateValidation,
   loginValidation,
+  userUpdateValidation,
 } = require("../middlewares/userValidation");
 const authGuard = require("../middlewares/authGuard");
+const { imageUpload } = require("../middlewares/imageUpload");
 
 // Routes
 router.post("/register", userCreateValidation(), validate, register);
 router.post("/login", loginValidation(), validate, login);
 router.get("/profile", authGuard, getCurrentUser);
+router.put(
+  "/",
+  authGuard,
+  userUpdateValidation(),
+  validate,
+  imageUpload.single("profileImage"),
+  update
+);
 
 module.exports = router;
