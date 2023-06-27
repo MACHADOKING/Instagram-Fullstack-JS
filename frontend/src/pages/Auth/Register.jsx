bb42dc833d2aeee834e707921a3f334a47ce2702
Julio Machado <jsmacbusiness@gmail.com>
2023-06-27 17:27:48 -0300

    Feat: creating registration form

--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -1,12 +1,30 @@
 // CSS
 import "./Auth.css";
-import React from "react";
+// packages
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div>
-      <h1>Register</h1>
-      <h2>Hello World!</h2>
+      <h2>
+        React<span>Gram</span>
+      </h2>
+      <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos.</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Nome" />
+        <input type="email" placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+        <input type="password" placeholder="Confirme a sua senha" />
+        <input type="submit" value="Cadastrar" />
+      </form>
+      <p>
+        Já tem uma conta? <Link to="/login">Clique aqui.</Link>
+      </p>
     </div>
   );
 };
