bed38e06e28f23dceced93af4943325ff49e149a
Julio Machado <jsmacbusiness@gmail.com>
2023-06-28 17:26:05 -0300

    Feat: creating login form

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,12 +1,45 @@
 // CSS
 import "./Auth.css";
-import React from "react";
+// Components
+import { Link } from "react-router-dom";
+import Message from "../../components/Message/Message";
+// hooks
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+// Redux
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-      <h2>Hello World</h2>
+    <div id="login">
+      <h2>
+        React<span>Gram</span>
+      </h2>
+      <p className="subtitle">Faça o login para ver o que há de novo.</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="E-mail"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email || ""}
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password || ""}
+        />
+        <input type="submit" value="Entrar" />
+      </form>
+      <p>
+        Não tem uma conta? <Link to="/register">Clique aqui</Link>
+      </p>
     </div>
   );
 };
