cf3c66f2e4b3d29f74ea7c57aed0c319e79a5963
Julio Machado <jsmacbusiness@gmail.com>
2023-06-28 16:53:52 -0300

    Feat: checking if the user is authenticated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@
 import "./App.css";
 // Router
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+// hooks
+import { useAuth } from "./hooks/useAuth";
 // components
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
@@ -11,15 +13,32 @@ import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
 
 function App() {
+  const { auth, loading } = useAuth();
+
+  console.log(loading);
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
+
   return (
     <main className="App">
       <BrowserRouter>
         <Navbar />
         <div className="container">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+            <Route
+              path="/"
+              element={auth ? <Home /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/login"
+              element={!auth ? <Login /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/register"
+              element={!auth ? <Register /> : <Navigate to="/" />}
+            />
           </Routes>
         </div>
         <Footer />
