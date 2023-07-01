6ead63ec50f0b4fec3ae3b22d9ea56cc27a48b23
Julio Machado <jsmacbusiness@gmail.com>
2023-07-01 16:54:21 -0300

    Feat: user photo rescue service

--- frontend/src/services/photoService.jsx
+++ frontend/src/services/photoService.jsx
@@ -14,8 +14,23 @@ const publishPhoto = async (data, token) => {
   }
 };
 
+// get user photos
+const getUserPhotos = async (id) => {
+  const config = requestConfig("GET");
+
+  try {
+    const res = await fetch(api + "/photos/users/" + id, config)
+      .then((res) => res.json())
+      .catch((err) => err);
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const photoService = {
   publishPhoto,
+  getUserPhotos,
 };
 
 export default photoService;
