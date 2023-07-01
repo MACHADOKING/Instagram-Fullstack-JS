413067f1bd78e4842406e6879a431cf969b04e34
Julio Machado <jsmacbusiness@gmail.com>
2023-07-01 13:37:43 -0300

    Feat: displaying data in profile

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -11,11 +11,38 @@ import Message from "../../components/Message/Message";
 import { Link } from "react-router-dom";
 import { BsFillEyeFill, BsPencilFill, BsXLg } from "react-icons/bs";
 // redux
+import { getUserDetails } from "../../slices/userSlice";
 
 const Profile = () => {
+  const { id } = useParams();
+
+  const dispatch = useDispatch();
+
+  const { user, loading } = useSelector((state) => state.user);
+  const { user: userAuth } = useSelector((state) => state.auth);
+
+  // photo
+
+  // load user data
+  useEffect(() => {
+    dispatch(getUserDetails(id));
+  }, [dispatch, id]);
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
+
   return (
-    <div>
-      <h1>Profile</h1>
+    <div id="profile">
+      <div className="profile-header">
+        {user.profileImage && (
+          <img src={`${upload}/users/${user.profileImage}`} alt={user.name} />
+        )}
+        <div className="profile-description">
+          <h2>{user.name}</h2>
+          <p>{user.bio}</p>
+        </div>
+      </div>
     </div>
   );
 };
