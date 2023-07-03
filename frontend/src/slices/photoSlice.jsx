de87340f50b477518d4f0b678b56cf3ba057ecc8
Julio Machado <jsmacbusiness@gmail.com>
2023-07-03 17:34:53 -0300

    Feat: photo slice by id

--- frontend/src/slices/photoSlice.jsx
+++ frontend/src/slices/photoSlice.jsx
@@ -77,6 +77,13 @@ export const updatePhoto = createAsyncThunk(
   }
 );
 
+// get photo by id
+export const getPhoto = createAsyncThunk("photo/getphoto", async (id) => {
+  const data = await photoService.getPhoto(id);
+
+  return data;
+});
+
 export const photoSlice = createSlice({
   name: "photo",
   initialState,
@@ -152,6 +159,16 @@ export const photoSlice = createSlice({
         state.loading = false;
         state.error = action.payload;
         state.photo = {};
+      })
+      .addCase(getPhoto.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(getPhoto.fulfilled, (state, action) => {
+        state.loading = false;
+        state.sucess = true;
+        state.error = null;
+        state.photo = action.payload;
       });
   },
 });
