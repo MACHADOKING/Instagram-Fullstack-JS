650c05d980c4027390ab78a994630b9a85532ec0
Julio Machado <jsmacbusiness@gmail.com>
2023-07-03 17:23:53 -0300

    Feat: creating individual photo page

---
+++ frontend/src/pages/Photo/Photo.jsx
@@ -0,0 +1,16 @@
+// CSS
+import "./Photo.css";
+// packages
+import { useEffect, useState } from "react";
+// components
+import Message from "../../components/Message/Message";
+import { Link, useParams } from "react-router-dom";
+// Redux
+import { useSelector, useDispatch } from "react-redux";
+import { upload } from "../../utils/config";
+
+const Photo = () => {
+  return <div>Photo</div>;
+};
+
+export default Photo;
