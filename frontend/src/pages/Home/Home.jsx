d3a93c3f40d609bd0000eb2dd091e99c9ff68141
Julio Machado <jsmacbusiness@gmail.com>
2023-07-06 18:26:22 -0300

    Feat: finishing home

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 // CSS
-import styles from "./Home.module.css";
+import "./Home.css";
 // components
 import LikeContainer from "../../components/LikeContainer";
 import PhotoItem from "../../components/PhotoItem";
@@ -14,7 +14,7 @@ import { getPhotos, like } from "../../slices/photoSlice";
 const Home = () => {
   const dispatch = useDispatch();
 
-  const resetMessage = useResetComponentMessage();
+  const resetMessage = useResetComponentMessage(dispatch);
 
   const { user } = useSelector((state) => state.auth);
   const { photos, loading } = useSelector((state) => state.photo);
@@ -36,9 +36,23 @@ const Home = () => {
   }
 
   return (
-    <div className={styles.home}>
-      <h1>Home</h1>
-      <h2>Hello World</h2>
+    <div id="home">
+      {photos &&
+        photos.map((photo) => (
+          <div key={photo._id}>
+            <PhotoItem photo={photo} />
+            <LikeContainer photo={photo} user={user} handleLike={handleLike} />
+            <Link className="btn" to={`/photos/${photo._id}`}>
+              Ver mais
+            </Link>
+          </div>
+        ))}
+      {photos && photos.length === 0 && (
+        <h2 className="no-photos">
+          Ainda não há fotos publicadas,{" "}
+          <Link to={`/users/${user._id}`}>Seja o primeiro a publicar.</Link>
+        </h2>
+      )}
     </div>
   );
 };
