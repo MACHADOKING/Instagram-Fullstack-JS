d95490f374391d5d4339957e6d87413a2eea5903
Julio Machado <jsmacbusiness@gmail.com>
2023-07-06 18:32:21 -0300

    Feat: Creating search page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import Register from "./pages/Auth/Register";
 import EditProfile from "./components/EditProfile/EditProfile";
 import Profile from "./pages/Profile/Profile";
 import Photo from "./pages/Photo/Photo";
+import Search from "./pages/Search/Search";
 
 function App() {
   const { auth, loading } = useAuth();
@@ -47,6 +48,10 @@ function App() {
             <Route
               path="/register"
               element={!auth ? <Register /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/search"
+              element={auth ? <Search /> : <Navigate to="/login" />}
             />
             <Route
               path="/photos/:id"
