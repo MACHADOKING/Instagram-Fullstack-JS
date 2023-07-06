e90169618d7412ec80112e0959df6b47156dcb10
Julio Machado <jsmacbusiness@gmail.com>
2023-07-06 18:01:33 -0300

    Feat: slice of all photos

--- frontend/src/slices/photoSlice.jsx
+++ frontend/src/slices/photoSlice.jsx
@@ -124,6 +124,17 @@ export const comment = createAsyncThunk(
   }
 );
 
+// Get all photos
+export const getPhotos = createAsyncThunk("photo/getall", async () => {
+  const data = await photoService.getPhotos();
+
+  if (data.errors) {
+    return thunkAPI.rejectWithValue(data.errors[0]);
+  }
+
+  return data;
+});
+
 export const photoSlice = createSlice({
   name: "photo",
   initialState,
@@ -242,6 +253,16 @@ export const photoSlice = createSlice({
       .addCase(comment.rejected, (state, action) => {
         state.loading = false;
         state.error = action.payload;
+      })
+      .addCase(getPhotos.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(getPhotos.fulfilled, (state, action) => {
+        state.loading = false;
+        state.sucess = true;
+        state.error = null;
+        state.photos = action.payload;
       });
   },
 });
