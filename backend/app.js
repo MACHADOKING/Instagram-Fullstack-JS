ff76eaee4b593ee22bfa9c425eb4136b9e3b3bb6
Julio Machado <jsmacbusiness@gmail.com>
2023-08-09 14:09:01 -0300

    Fix: Cors

--- backend/app.js
+++ backend/app.js
@@ -14,15 +14,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Configurar política de CORS
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://reactgram-fullstack.vercel.app"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader(
+//     "Access-Control-Allow-Origin",
+//     "https://reactgram-fullstack.vercel.app"
+//   );
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
 
 // Solve cors
 app.use(
